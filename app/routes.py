f6de60350994212dae6bcce7a9dd663fbf466527
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2020-02-19 20:20:00 +0100

    adding the forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
@@ -14,3 +14,11 @@ def index():
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'}]
     return render_template("index.html",title="Homepage",user=user,posts=posts)
+
+
+@app.route('/login')
+def login():
+
+    form= LoginForm()
+    return render_template('login.html',title='Login Page',form=form)
+
