4d7a291a676944af2bb3b25bce1933e4fe25eead
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2020-02-20 15:36:42 +0100

    adding for validation and Post request handling

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,flash, redirect , url_for
 from app import app
 from app.forms import LoginForm
 @app.route('/')
@@ -18,7 +18,9 @@ def index():
 
 @app.route('/login')
 def login():
-
     form= LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for the user {}, remeber me {}'.format(form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html',title='Login Page',form=form)
 
