bfa5ac3b4279f1acca54fcd2c0560549477326c3
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2020-02-27 16:54:37 +0100

    setting up config for migration

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
 import os
+basedir= os.path.abspath(os.path.dirname(__file__))
 class Config():
     SECRET_KEY= os.environ.get('SECRET_KEY') or "Try to guess haha"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
