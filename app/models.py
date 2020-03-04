85ff222bc8aceefcb4a6c4c6a79991f174a3d6a9
elhaidi abdessalam <abdessalam.elhaidi@gmail.com>
2020-03-04 19:42:57 +0100

    adding the logg-in section

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64),index=True,unique=True) 
@@ -14,10 +15,10 @@ class User(db.Model):
         return '< USER {}>'.format(self.username )
     
     def set_password(self,password):
-        self.passwword_hash=generate_password_hash(password)
+        self.password_hash=generate_password_hash(password)
     
     def check_password(self,password):
-        return check_password_hash(self.passwword_hash,password)
+        return check_password_hash(self.password_hash,password)
         
 class Post(db.Model):
 
@@ -28,3 +29,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
